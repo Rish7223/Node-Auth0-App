1a611428eb4eea8631fa8bb6040e87ae59de26cd
Rish7223 <rishabh.7223@gmail.com>
2021-05-19 22:58:19 +0530

    added views using pug

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 require('dotenv').config();
 const express = require('express');
+const path = require('path');
 const authConfig = require('./middleware/auth0');
 const routes = require('./routes');
 const app = express();
+
+// pug setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+//setting up static files
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(authConfig);
 app.use('/', routes);
 
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
-  console.log('server is listing on port 400');
+  console.log('server is listing on port 4000');
 });
--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,49 @@
 const express = require('express');
 const Router = express.Router();
 const { requiresAuth } = require('express-openid-connect');
+const NewsApi = require('newsapi');
+
+const newsapi = new NewsApi(process.env.NEWS_API_KEY);
 
 Router.get('/', (req, res) => {
-  res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
+  const isAuthenticated = req.oidc.isAuthenticated();
+  if (isAuthenticated) {
+    res.render('home', {
+      user: req.oidc.user,
+      isAuthenticated: true,
+    });
+    return;
+  }
+  res.render('home', { isAuthenticated: false });
 });
 
 Router.get('/profile', requiresAuth(), (req, res) => {
   const data = req.oidc.user;
-  console.log(data);
   res.send(JSON.stringify(data));
+});
+
+Router.get('/news', requiresAuth(), async (req, res) => {
+  res.status(200).render('categories', {
+    isAuthenticated: req.oidc.isAuthenticated(),
+    user: req.oidc.user,
+  });
+});
+
+Router.get('/news/:category', requiresAuth(), async (req, res) => {
+  try {
+    const id = req.params.category;
+    const response = await newsapi.v2.sources({
+      category: id,
+      language: 'en',
+      country: 'us',
+    });
+    res.status(200).send(JSON.stringify(response));
+  } catch (error) {
+    if (error) {
+      console.log(error);
+      res.status(400).send('News api request failed');
+    }
+  }
 });
 
 module.exports = Router;
